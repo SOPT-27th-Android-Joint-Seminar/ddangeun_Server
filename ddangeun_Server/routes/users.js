e89b365f450cb453949b9377be953a7988d28ff5
윤가영 <rkdkdudjd@gmail.com>
2020-12-05 17:36:19 +0900

    c

--- ddangeun_Server/routes/users.js
+++ ddangeun_Server/routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../modules/pool');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/productList', async (req, res) => {
+  try {
+    const query = 'SELECT productName, createdAt, price, likeNum, commentNum FROM PRODUCT_TB;'
+    const data = await pool.queryParam(query);
+    console.log(data);
+
+    return res
+      .status(200)
+      .send(data);
+  } catch (err) {
+    console.log('productList error: ', err);
+    res
+      .status(500)
+      .send({status: 500});
+  }
+})
+
 
 module.exports = router;
