964f38e31b999f30df5c48c2a3362f0660a13df7
Kayoung <rkdkdudjd@naver.com>
2020-12-06 14:00:51 +0900

    users.js 리팩토링

--- ddangeun_Server/routes/users.js
+++ ddangeun_Server/routes/users.js
@@ -12,7 +12,7 @@ router.get('/productList', async (req, res) => {
     const query = 'SELECT PRODUCT_TB.*, USER_TB.local FROM PRODUCT_TB INNER JOIN USER_TB ON PRODUCT_TB.user_idx = USER_TB.idx;'
     const data = await pool.queryParam(query);
 
-    const dataWithImgPromises = data.map(async product => {
+    const dataWithImg = await Promise.all(data.map(async product => {
       const images = await pool.queryParam(`SELECT * FROM PRODUCT_IMG_TB WHERE product_idx=${product.idx};`);
       if(images.length === 0){
         return product;
@@ -20,12 +20,7 @@ router.get('/productList', async (req, res) => {
         product.imgLink = images[0].imgLink;
         return product;
       }
-    })
-
-    const dataWithImg = [];
-    for(let i = 0; i < dataWithImgPromises.length ; i++){
-      dataWithImg.push(await dataWithImgPromises[i]);
-    }
+    }))
 
     return res
       .status(200)
